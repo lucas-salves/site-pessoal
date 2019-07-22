22d4f47876587e8e9a7d1dbf2a778039951e6152
unknown <Administrador@50-DESK61372.preventsenior.local>
2019-07-22 05:53:14 -0300

    otimizacao lazy load com css

--- homepage-scroll.js
+++ homepage-scroll.js
@@ -1,64 +1,23 @@
-	let btnDownArrows = document.getElementsByClassName('scroll-down-arrows');
-	
 window.onload = function(){
-	let titleService = document.getElementById('service-title');
-	let servicesAreaContainer = document.getElementById('services-area-container');
-	
-	//logica dos botoes de scroll
-	btnDownArrows[0].addEventListener("click", function(){
-		document.getElementById('services-area').scrollIntoView({
-			behavior: 'smooth'
-		});
-		setTimeout(fadeScrollElements,300);
-	});
-	
-	setTimeout(incrementOpacity,500);
-
-	let	flag = true;
-	window.addEventListener("scroll",function(){
-		if(window.pageYOffset > 680){							
-			if (flag) {
-				flag = false;				
-				fadeScrollElements();
-			}
-			
-		}
-	});
-
-let iFade = 0;
-	function fadeScrollElements(){						
-		
-		if(iFade < 10){
-			setInterval(fadeScrollElements,150);
-		}else{
-			clearInterval();
-			return false;
-		}
-		titleService.style.opacity = '0.'+iFade;
-		servicesAreaContainer.style.opacity = '0.'+iFade;
-		iFade++;
-		
-		
-	}
-	
+    let servicesAreaContainer = document.getElementById('services-area-container');
+    let serviceTitle = document.getElementById('service-title');
+    let btnDownArrows = document.getElementsByClassName('scroll-down-arrows');
+    btnDownArrows[0].addEventListener("click", function(){
+        document.getElementById('services-area').scrollIntoView({behavior: 'smooth'});
+        
+    });
+    
+    let flag = true;
+    window.addEventListener("scroll",scrollFadeElements);
+    function scrollFadeElements(){
+        if(window.pageYOffset > 680){
+           if(flag){
+                flag = false;               
+               serviceTitle.style.animation = 'services 1s';
+                servicesAreaContainer.style.animation = 'services 3s';
+               serviceTitle.style.opacity = '1';
+               servicesAreaContainer.style.opacity = '1';
+            }
+        }
+    }
 }
-	
-	
-	/*
-		funcao efeito transition no botao de scroll do header quando a pagina carrega
-	*/
-	let iOpacity = 0;
-	function incrementOpacity(){		
-		if (iOpacity < 10 ) {			
-			setInterval(incrementOpacity,150);			
-		}else{			
-			clearInterval();			
-			return false;
-		}
-		
-		btnDownArrows[0].style.opacity = '0.'+iOpacity;
-		iOpacity++;
-		
-	}
-
-	
