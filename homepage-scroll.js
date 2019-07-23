e58e6a146306aa0929994661eb62370f3652b2e7
unknown <Administrador@50-DESK61372.preventsenior.local>
2019-07-23 05:29:14 -0300

    otimizacao dos scripts de lazy load

--- homepage-scroll.js
+++ homepage-scroll.js
@@ -2,19 +2,41 @@ window.onload = function(){
     let servicesAreaContainer = document.getElementById('services-area-container');
     let serviceTitle = document.getElementById('service-title');
     let btnDownArrows = document.getElementsByClassName('scroll-down-arrows');
+    
+    let siteTitle = document.getElementById('site-title');
+    let siteDescription = document.getElementById('site-description');
+    let avatarImage = document.getElementById('avatar-image');
+    let aboutText = document.getElementById('about-text');
+    let headerSliderButtons = document.getElementById('header-slider-buttons');
+    
     btnDownArrows[0].addEventListener("click", function(){
         document.getElementById('services-area').scrollIntoView({behavior: 'smooth'});
         
     });
     
     let flag = true;
-    window.addEventListener("scroll",scrollFadeElements);
+   
+
+    setTimeout( function(){selectElementToFade(siteTitle,1);},300 );
+    setTimeout( function(){selectElementToFade(siteDescription,1);},375 );
+    setTimeout( function(){selectElementToFade(avatarImage,1);},400 );
+    setTimeout( function(){selectElementToFade(aboutText,1);},440 );
+    setTimeout( function(){selectElementToFade(btnDownArrows[0],1);},600 );
+    setTimeout( function(){selectElementToFade(headerSliderButtons,1);},640  );
+
+       function selectElementToFade(element, duration){     
+       console.log('oi');
+       element.style.animation = 'lazyLoadFade '+duration+"s";
+    element.style.opacity = '1';
+   }
+   
+    window.addEventListener("scroll", scrollFadeElements);
     function scrollFadeElements(){
         if(window.pageYOffset > 680){
            if(flag){
                 flag = false;               
-               serviceTitle.style.animation = 'services 1s';
-                servicesAreaContainer.style.animation = 'services 3s';
+               serviceTitle.style.animation = 'lazyLoadFade 1s';
+                servicesAreaContainer.style.animation = 'lazyLoadFade 3s';
                serviceTitle.style.opacity = '1';
                servicesAreaContainer.style.opacity = '1';
             }
